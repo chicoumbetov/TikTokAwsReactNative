1228eb54627ac99646b0d2afdb1779bb84077ced
chicoumbetov <shyngysmaaan@gmail.com>
2021-03-16 21:20:31 +0100

    Pause by clicking on video itself implementation

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,42 +1,38 @@
-import React, {useRef, useState} from 'react'
-import { View, Button } from 'react-native'
+import React, { useRef, useState } from 'react'
+import { View, Button, TouchableWithoutFeedback } from 'react-native'
 import { Video } from 'expo-av';
 import styles from './styles';
-//import kz from '../../assets/video/kz.mp4'
 
 const Post = () => {
+
+    const [paused, setPaused] = useState({})
+
+    const onPlayPausePress = () => {
+        console.warn('console.warn(Clicked on video to pause)')
+        //setPaused(!paused);
+        paused.isPlaying
+            ? videoRef.current.pauseAsync()
+            : videoRef.current.playAsync()
+    }
     const videoRef = useRef(null);
-    const [status, setStatus] = useState({});
     return (
-        <View style={styles.container}>
-            <Video
-                ref={videoRef}
-                style={styles.video}
-                source={require('../../assets/video/kz.mp4')}
-                useNativeControls
-                //onError={(e)=> console.log(e)}
-                resizeMode="cover"
-                isLooping
-                onPlaybackStatusUpdate={status => setStatus(() => status)}
-            />
-
-            
-            <View style={{marginTop: 20}}>
-                <Button
-                    title={status.isPlaying 
-                        ? 'Pause' 
-                        : 'Play'
-                    }
-                    onPress={() =>
-                        status.isPlaying 
-                            ? videoRef.current.pauseAsync() 
-                            : videoRef.current.playAsync()
-                    }
-                />
-            </View>
-            
-
-        </View>
+        <>
+            <TouchableWithoutFeedback onPress={onPlayPausePress}>
+                <View style={styles.container}>
+                    <Video
+                        ref={videoRef}
+                        style={styles.video}
+                        source={require('../../assets/video/kz.mp4')}
+                        //useNativeControls
+                        onError={(e) => console.log(e)}
+                        resizeMode="cover"
+                        shouldPlay
+                        isLooping
+                        onPlaybackStatusUpdate={paused => setPaused(() => paused)}
+                    />
+                </View>
+            </TouchableWithoutFeedback>
+        </>
     )
 }
 
