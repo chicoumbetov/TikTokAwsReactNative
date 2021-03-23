fdec4d99bc71507749a4681110b5f2f910f4f036
chicoumbetov <shyngysmaaan@gmail.com>
2021-03-23 12:20:42 +0100

    connect signed up Cognito User with Database user

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
+import { useEffect } from 'react';
+
 import { StyleSheet, View } from 'react-native';
 //import BirdGame from './src/components/BirdGame';
 //import Home from './src/screens/HomeScreen/HomeScreen';
 import Navigation from './src/navigation/Navigation';
 
-import { Auth, API, graqlOperation } from 'aws-amplify';
+import { Auth, API, graqlOperation, graphqlOperation } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react-native';
 
 import { createUser } from './src/graphql/mutations'
+import { getUser } from './src/graphql/queries';
 
 import Amplify from 'aws-amplify'
 import config from './src/aws-exports'
-import { useEffect } from 'react';
-Amplify.configure(config)
+
+const randomImages = [
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg',
+];
+
+const getRandomImage = () => {
+  return randomImages[Math.floor( Math.random() * randomImages.length )]
+}
+
+Amplify.configure({ 
+  ...config,
+  Analytics: {
+    disabled: true,
+  }
+})
 
 function App() {
   console.log("App")
@@ -24,10 +43,40 @@ function App() {
       const userInfo = await Auth.currentAuthenticatedUser({ bypassCache: true })
       console.log(userInfo)
 
+      if (!userInfo) {
+        return;
+      }
       //check if the user exist in the database
+      const getUserResponse = await API.graphql(
+        graphqlOperation(
+          getUser,
+          { id: userInfo.attributes.sub}
+        )
+      )
+
+      if (getUserResponse.data.getUser) {
+        console.log("User already exists in database");
+        return;
+      }
 
       // if it doesn't (it's newly registered user)
       // then, create a new user in database
+      const newUser = {
+        id: userInfo.attributes.sub,
+        username: userInfo.username,
+        email: userInfo.attributes.email,
+        
+        imageUri: getRandomImage(),
+      };
+      console.log(newUser)
+
+      //send newUser to API
+      await API.graphql(
+        graphqlOperation(
+          createUser,
+          { input: newUser }
+        )
+      )
     }
 
     fetchUser();
